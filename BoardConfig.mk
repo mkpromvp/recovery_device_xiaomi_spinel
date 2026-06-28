80e02c71e112cc06a40e14414ee08a2760339799
MK VENOM PRO <116007409+mkpromvp@users.noreply.github.com>
2026-06-28 22:11:45 +0300

    fix casing: device/Xiaomi -> device/xiaomi in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/Xiaomi/spinel
+DEVICE_PATH := device/xiaomi/spinel
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
